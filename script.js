13d65a42f14b99461f3ef8371ba0372ceab8fcbd
mariani2008 <160234697+mariani2008@users.noreply.github.com>
2025-05-07 11:37:06 +0000

    terminamos aula 5

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -7,15 +10,16 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
     
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+       })
+    
 });
-
-
     
